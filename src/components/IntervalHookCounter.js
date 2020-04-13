4d9bc97cc6afc13eafe44aea15d0be2d3e7eee80
joffitzer <joffitzer@gmail.com>
2020-04-13 10:30:48 -0400

    dependency list changes when prevCount used in setCount function

--- src/components/IntervalHookCounter.js
+++ src/components/IntervalHookCounter.js
@@ -5,16 +5,23 @@ function IntervalHookCounter() {
     const [count, setCount] = useState(0)
 
     const tick = () => {
-        setCount(count + 1)
+        setCount(prevCount => prevCount + 1)
     }
 
     useEffect(() => {
+
+        function doSomething() {
+            console.log(someProp)
+        }
+
+        doSomething()
+
         const interval = setInterval(tick, 1000)
 
         return () => {
             clearInterval(interval)
         }
-    }, [count])
+    }, [someProp])
 
     return(
         <div>
