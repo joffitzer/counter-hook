f562f860a4dfb1ade1c0f85b40332c7868ed6726
joffitzer <joffitzer@gmail.com>
2020-04-17 15:09:43 -0400

    simple useContext hook example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import './App.css';
-import ClassCounter from './components/ClassCounter'
-import ClassCounterTwo from './components/ClassCounterTwo'
-import HookCounter from './components/HookCounter'
-import HookCounterTwo from './components/HookCounterTwo';
-import HookCounterThree from './components/HookCounterThree';
-import HookCounterFour from './components/HookCounterFour';
-import HookCounterFive from './components/HookCounterFive';
-import ClassMouse from './components/ClassMouse'
-import HookMouse from './components/HookMouse'
-import MouseContainer from './components/MouseContainer';
-import IntervalClassCounter from './components/IntervalClassCounter'
-import IntervalHookCounter from './components/IntervalHookCounter';
-import DataFetching from './components/DataFetching'
+// import ClassCounter from './components/useEffect-and-useState/ClassCounter'
+// import ClassCounterTwo from './components/useEffect-and-useState/ClassCounterTwo'
+// import HookCounter from './components/useEffect-and-useState/HookCounter'
+// import HookCounterTwo from './components/useEffect-and-useState/HookCounterTwo';
+// import HookCounterThree from './components/useEffect-and-useState/HookCounterThree';
+// import HookCounterFour from './components/useEffect-and-useState/HookCounterFour';
+// import HookCounterFive from './components/useEffect-and-useState/HookCounterFive';
+// import ClassMouse from './components/useEffect-and-useState/ClassMouse'
+// import HookMouse from './components/useEffect-and-useState/HookMouse'
+// import MouseContainer from './components/useEffect-and-useState/MouseContainer';
+// import IntervalClassCounter from './components/useEffect-and-useState/IntervalClassCounter'
+// import IntervalHookCounter from './components/useEffect-and-useState/IntervalHookCounter';
+// import DataFetching from './components/useEffect-and-useState/DataFetching'
+import ComponentC from './components/ComponentC'
+
+export const UserContext = React.createContext()
 
 function App() {
   return (
     <div className="App">
-      <DataFetching />
+      <UserContext.Provider value={'jonah'}>
+        <ComponentC />
+      </UserContext.Provider>
     </div>
   );
 }
